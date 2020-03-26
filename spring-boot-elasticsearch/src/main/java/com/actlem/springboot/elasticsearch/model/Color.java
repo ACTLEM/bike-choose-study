5b6d6967b648542fe85f07e8bb637c33f00ff1de
ACTLEM <thomas.lemaire.actlem@gmail.com>
2020-03-26 15:08:45 +0100

    feat(sb-es): Add colors to the model

---
+++ spring-boot-elasticsearch/src/main/java/com/actlem/springboot/elasticsearch/model/Color.java
@@ -0,0 +1,28 @@
+package com.actlem.springboot.elasticsearch.model;
+
+import lombok.Getter;
+
+/**
+ * Color of a {@link Bike}. A {@link Bike} can have several {@link Color}
+ */
+public enum Color {
+
+    BLACK("Black"),
+    BLUE("Blue"),
+    BROWN("Brown"),
+    GREEN("Green"),
+    GREY("Grey"),
+    ORANGE("Orange"),
+    PINK("Pink"),
+    PURPLE("Purple"),
+    RED("Red"),
+    WHITE("White"),
+    YELLOW("Yellow");
+
+    @Getter
+    private String label;
+
+    Color(String label) {
+        this.label = label;
+    }
+}
